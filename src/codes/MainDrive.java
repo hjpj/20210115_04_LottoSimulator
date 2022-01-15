09ad33cfd30d60ef045393bce47c4f6a54ed7bac
hjpj <hoj524952@gmail.com>
2022-01-15 12:15:01 +0900

    중복 검사 기능 구현
    -for each 문을 이용해서 내 숫자들을 꺼내보고 / 입력 숫자와 비교

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -35,6 +35,23 @@ public class MainDrive {
 				
 //				2. 이미 등록된 숫자가 아닌가? => 중복이 아닌가?
 				
+				boolean isDuplOk = true; // 일단 써도 된다고 전제 -> 중복임을 발견하면, 쓰면 안된다고 변경.
+				
+//				내 번호목록을 돌아보다가 => 입력한 숫자와 같은게 있다면> => 이미 등록된 숫자. (중복) => 사용하면 안됨.
+				
+				for(int myNum : myNumberArr) {
+					
+					if(myNum == inputNum) {
+						
+						
+						isDuplOk = false;
+						
+						
+//						더 이상 검사할 필요 X
+						break;
+					}
+				}
+				
 //				1, 2를 모두 통과하면 => 내 번호 배열에 등록 + 다음 숫자로 이동.
 				
 				if (isRangeOk) {
@@ -60,11 +77,7 @@ public class MainDrive {
 			
 //			몇 번째 숫자? 안내문.
 			
-			System.out.print((i+1) + "번째 숫자 입력 : ");
-			
-//			숫자 입력받기.
-			
-			int inputNum = myScanner.nextInt();
+
 			
 		}
 		
