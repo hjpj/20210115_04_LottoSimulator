bf7fc818a92ebbdd77e72fd7b4c6fe132b6f778f
hjpj <hoj524952@gmail.com>
2022-01-15 12:10:34 +0900

    임시 - 범위검사만 통과해도 다음숫자로 넘어가게.

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -17,6 +17,7 @@ public class MainDrive {
 		Scanner myScanner = new Scanner(System.in);
 		
 		for (int i = 0; i< myNumberArr.length ; i++) {
+
 			
 //			제대로 된 숫자가 들어올 때까지 -> 각 숫자를 계속 반복으로 받아야함.
 			
@@ -36,16 +37,34 @@ public class MainDrive {
 				
 //				1, 2를 모두 통과하면 => 내 번호 배열에 등록 + 다음 숫자로 이동.
 				
+				if (isRangeOk) {
+					
+//					검사 통과 상황.
+					
+//					내 번호 목록에 -> 입력한 숫자를 기록.
+					myNumberArr[i] = inputNum;
+					
+//					무한반복 종료 -> 다음 숫자로 이동.
+					break;
+					
+					
+				}
+				
 				
 				
 			}
 		
 			
+
 //			내 번호를 자리에 입력받아 넣자. => Scanner 필요. (밖에서 생성)
 			
 //			몇 번째 숫자? 안내문.
 			
-
+			System.out.print((i+1) + "번째 숫자 입력 : ");
+			
+//			숫자 입력받기.
+			
+			int inputNum = myScanner.nextInt();
 			
 		}
 		
