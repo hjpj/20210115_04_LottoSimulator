d428aa55b2c73e368a559965b9d698ba58c74295
hjpj <hoj524952@gmail.com>
2022-01-15 12:03:06 +0900

    Update MainDrive.java

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -18,15 +18,26 @@ public class MainDrive {
 		
 		for (int i = 0; i< myNumberArr.length ; i++) {
 			
+//			제대로 된 숫자가 들어올 때까지 -> 각 숫자를 계속 반복으로 받아야함.
+			
+			while(true) {
+				
+				System.out.print((i+1) + "번째 숫자 입력 : ");
+				
+//				숫자 입력받기.
+				
+				int inputNum = myScanner.nextInt();
+				
+				
+				
+			}
+		
+			
 //			내 번호를 자리에 입력받아 넣자. => Scanner 필요. (밖에서 생성)
 			
 //			몇 번째 숫자? 안내문.
 			
-			System.out.print((i+1) + "번째 숫자 입력 : ");
-			
-//			숫자 입력받기.
-			
-			int inputNum = myScanner.nextInt();
+
 			
 		}
 		
